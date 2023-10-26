40c04bb195a547187b5e61e85d7c344ca25f73f2
Victor Kayk <victorkayk77@gmail.com>
2023-10-26 20:07:58 -0300

    feat: add indexs for products, categories and images ids

---
+++ src/main/resources/db/migration/V4__add_indexs_for_products_categories_images_ids.sql
@@ -0,0 +1,8 @@
+-- Index for table products
+CREATE INDEX idx_products_id ON products (id);
+
+-- Index for table categories
+CREATE INDEX idx_categories_id ON categories (id);
+
+-- Index for table images
+CREATE INDEX idx_images_id ON images (id);
